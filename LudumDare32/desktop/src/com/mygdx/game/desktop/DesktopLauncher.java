5e70e5e7ea80a26cb258cbb9fb941f1780d16b54
spyispie120 <ducnguyenduchao@gmail.com>
2015-12-11 19:04:15 -0800

    -Recommit

--- LudumDare32/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ LudumDare32/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.LudumDare34;
+import com.mygdx.game.LudumDare32;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LudumDare34(), config);
+		new LwjglApplication(new LudumDare32(), config);
 	}
 }
