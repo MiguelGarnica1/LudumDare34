111102e246af578588cf67f04f25b7ef40df6f24
Miguel Garnica <mgarnica123@aol.com>
2015-12-12 13:10:04 -0800

    Added Sushi path movement, and position checker

--- LudumDare32/core/src/com/mygdx/game/HUD.java
+++ LudumDare32/core/src/com/mygdx/game/HUD.java
@@ -30,7 +30,7 @@ public class HUD {
 	}
 
 	public void render(Batch batch) {
-		hudSush.setPosition(position);
+		hudSush.setPosition(position.x, position.y);
 		hudSush.render(batch);
 	}
 
--- LudumDare32/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ LudumDare32/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.width = 1024;
-		config.height = 576;
+		config.width = 720;
+		config.height = 480;
 		config.resizable = false;
 		
 		new LwjglApplication(new LudumDare32(), config);
